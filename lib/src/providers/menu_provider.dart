22f945762b7e70994734a65e43d39987944f8e4b
JoseCima <josegcima@gmail.com>
2022-02-20 13:52:25 -0500

    Convertir un JSONString en un map

--- lib/src/providers/menu_provider.dart
+++ lib/src/providers/menu_provider.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/services.dart' show rootBundle;
-
+import 'dart:convert';
 
 
 class _MenuProvider{
   List<dynamic> opciones =[];
 
   _MenuProvider(){
-    cargarData();
+    //cargarData();
   }
 
-  cargarData() {
+  Future<List<dynamic>>cargarData() async {
 
-   rootBundle.loadString('data/menu_opts.json')
-   .then( (data){
-   print(data);
-   });
+  final resp = await rootBundle.loadString('data/menu_opts.json');
+   
+   Map dataMap = json.decode(resp);
+   print(dataMap['nombreApp']);//Nombre de la propiedad
+   opciones =dataMap['rutas'];
+
+   return opciones;
 }
 
 }
