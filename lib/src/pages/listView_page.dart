55443895ff7af9bed235aa6687610de115d2a9a1
JoseCima <josegcima@gmail.com>
2022-02-21 18:47:52 -0500

    infiniteScroll

--- lib/src/pages/listView_page.dart
+++ lib/src/pages/listView_page.dart
@@ -8,7 +8,27 @@ class ListaPage extends StatefulWidget {
 
 class _ListaPageState extends State<ListaPage> {
 
-  List<int> _listaNumeros = [1,2,3,4,5];
+  ScrollController _scrollController = new ScrollController();
+
+ 
+
+   
+  List<int> _listaNumeros = [];
+  int _ultimoItem = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _agregar10();
+
+    _scrollController.addListener(() {
+       if(_scrollController.position.pixels == _scrollController.position.maxScrollExtent){
+         _agregar10();
+       }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +43,8 @@ class _ListaPageState extends State<ListaPage> {
   
   Widget _crearLista(){
      return ListView.builder(
+
+       controller: _scrollController,
         itemCount: _listaNumeros.length,
         itemBuilder: (BuildContext context, int index){
 
@@ -30,12 +52,25 @@ class _ListaPageState extends State<ListaPage> {
           final imagen = _listaNumeros[index];
             return FadeInImage(
               placeholder: AssetImage('assets/loading.gif'),
-               image: NetworkImage('https://picsum.photos/seed/picsum/500/300/?image=$imagen'),
+               image: NetworkImage('https://picsum.photos/500/300/?image=$index'),
                );
         },
      );
   }
 
+
+  void _agregar10(){
+    for (var i = 1; i < 10; i++) {
+      _ultimoItem++;
+      _listaNumeros.add(_ultimoItem);
+
+
+    }
+
+    setState(() {
+      
+    });
+  }
 
 
 }
