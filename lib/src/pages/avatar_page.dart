00d7a042356aabc57bfa3849cffdd050eacadcfc
JoseCima <josegcima@gmail.com>
2022-02-21 11:53:06 -0500

    Widget - Circle Avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -7,6 +7,35 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+
+
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://data.whicdn.com/images/341962388/original.jpg'),
+              radius: 25.0,
+            ),
+          ),
+         
+
+          Container(
+            margin: EdgeInsets.only(right:10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.pink,
+              
+            ),
+          )
+        ],
+      ),
+      body:Center(
+        child:FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://media-cdn.laodong.vn/storage/newsportal/2021/3/14/888920/Rose.JPG?w=960&crop=auto&scale=both'),
+          fadeInDuration: Duration(milliseconds: 200),
+          ),
+        
       ),
     );
   }
